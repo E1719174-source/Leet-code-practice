f9acafc8d9d999f1b7a7c730f5113740888dd547
elia <WangytElia@163.com>
2026-03-16 09:41:20 +0800

    3.16

---
+++ 3.16/3.16/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  3.16
+//
+//  Created by 王钰童 on 2026/3/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
