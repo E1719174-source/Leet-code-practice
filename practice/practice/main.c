06795e26ccf6428ba0b9e9b8a8a8143f6adbf0c3
elia <WangytElia@163.com>
2026-03-16 09:41:01 +0800

    完成3.16刷题任务

---
+++ practice/practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  practice
+//
+//  Created by 王钰童 on 2026/3/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf(", World!\n");
+    return 0;
+}
